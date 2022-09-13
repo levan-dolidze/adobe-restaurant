30787c47c00bdc58c8b80897c8ce142f41f03c25
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2022-09-13 14:02:58 +0300

    update

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+    this.returnCategory();
+  };
+
+  returnCategory(): string | null {
+    const category = this.route.snapshot.paramMap.get('category')
+    return category
+  };
 
 }
