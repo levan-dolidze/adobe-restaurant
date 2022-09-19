4bcbec2e106e6c8a4eb25c72522c654b94518d5d
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2022-09-20 01:31:32 +0400

    update

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Menu } from 'src/app/models/menu';
 import { HttpService } from 'src/app/services/http.service';
+import { fade } from 'src/app/shared/animations';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  animations:[fade]
 })
 export class MenuComponent implements OnInit {
 
