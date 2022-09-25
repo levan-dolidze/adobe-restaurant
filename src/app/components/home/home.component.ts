4cae6b8542705ced948d4839071a5545f4caee77
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2022-09-25 19:33:15 +0400

    update

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,17 +4,14 @@ import { HttpService } from 'src/app/services/http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css','./home.component.scss']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
   constructor(private http: HttpService) { }
 
   ngOnInit(): void {
- 
-    
 
+  }
 
-  };
-
-};
+}
